a5d66d19e5469c29879958c09847f1d9776c4946
Neeraj Kumar <Neeraj.Kumar02a@gmail.com>
2020-06-30 20:54:10 +0530

    Compleated!

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [
+STATIC_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static/')
 ]
 
